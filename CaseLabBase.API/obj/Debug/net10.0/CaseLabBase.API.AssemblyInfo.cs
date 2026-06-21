e88da9d07d784e1f77d97652a39afc4fe8a148d2
hgd631 <hgd631@uregina.ca>
2026-06-20 21:30:21 -0600

    Implement - function loadQuizzesSidebar _ renderInlineQuestionEditSection _ instructorPublishTask

--- CaseLabBase.API/obj/Debug/net10.0/CaseLabBase.API.AssemblyInfo.cs
+++ CaseLabBase.API/obj/Debug/net10.0/CaseLabBase.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CaseLabBase.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce72fc820a743dfefd4d025882b2a126226a7153")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+50f43ba20199b8005dcb69561c26d22d41c12be9")]
 [assembly: System.Reflection.AssemblyProductAttribute("CaseLabBase.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CaseLabBase.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
