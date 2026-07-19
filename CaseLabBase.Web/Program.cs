d1dcc8fbdc085f1cb819c9071a324eb4aac4826f
hgd631 <hgd631@uregina.ca>
2026-07-18 21:23:48 -0600

    api added

--- CaseLabBase.Web/Program.cs
+++ CaseLabBase.Web/Program.cs
@@ -6,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 
 // Add services to the container (register MVC views & controllers)
+builder.Services.AddControllers();
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
